5af40a394745852f44274d11e121f7564e41f433
Eduardo Seiji Suguimoto Miyazato Ferrer <eduardo.seiji@usp.br>
2023-12-07 10:16:13 -0300

    Update views.py

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+@api_view(['GET'])
+def getOrder(request):
+    app = Order.objects.all()
+    serializer = DataSerializer(app, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def postOrder(request):
+    serializer = DataSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
+
+@api_view(['PUT'])
+def updateOrder(request, pk):
+    try:
+        app = Order.objects.get(pk=pk)
+    except Order.DoesNotExist:
+        return Response({'error': 'Order not found'}, status=404)
+
+    serializer = DataSerializer(app, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
+
+@api_view(['DELETE'])
+def deleteOrder(request, pk):
+    try:
+        app = Order.objects.get(pk=pk)
+    except Order.DoesNotExist:
+        return Response({'error': 'Order not found'}, status=404)
+
+    app.delete()
+    return Response(status=204)
