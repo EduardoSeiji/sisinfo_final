319dce3f0f1da139e18444f82810b9c162f970e0
Eduardo Seiji Suguimoto Miyazato Ferrer <eduardo.seiji@usp.br>
2023-12-06 22:24:40 -0300

    Update serializer.py

--- apps/product/serializer.py
+++ apps/product/serializer.py
@@ -4,4 +4,4 @@ from .models import Product
 class DataSerializer(serializers.ModelSerializer):
     class Meta:
         model=Product
-        fields=('name','destino_url','descricao','coordenadas','preco','categoria','empresaid')
+        fields=('category','seller','title')
