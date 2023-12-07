3f7030276bb48712874ee220c7ab23a7305a985d
Eduardo Seiji Suguimoto Miyazato Ferrer <eduardo.seiji@usp.br>
2023-12-06 22:30:03 -0300

    Update urls.py

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('search/', views.search, name='search'),
     path('<slug:category_slug>/<slug:product_slug>/', views.product, name='product'),
-    path('<slug:category_slug>/', views.category, name='category')
+    path('<slug:category_slug>/', views.category, name='category'),
+    path('get_all', views.getProduct),
+    path('post/',views.postProduct),
+    path('put/<int:pk>/', views.updateProduct),
+    path('delete/<int:pk>/', views.deleteProduct)
 ]
